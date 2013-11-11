7576632e6be11a30ed73c3f6aeb014caf43a5149
Tim Schaub <tim.schaub@gmail.com>
2013-11-11 12:15:32 -0700

    Times and mode for file system items

--- test/lib/item.spec.js
+++ test/lib/item.spec.js
@@ -66,4 +66,91 @@ describe('Item', function() {
 
   });
 
+  describe('#getATime()', function() {
+
+    it('returns a date', function() {
+      var item = new Item('foo');
+      var date = item.getATime();
+      assert.instanceOf(date, Date);
+      assert.isTrue(date <= new Date());
+    });
+
+  });
+
+  describe('#setATime()', function() {
+
+    it('sets the atime', function() {
+      var item = new Item('foo');
+      var date = new Date();
+      item.setATime(date);
+      assert.equal(item.getATime(), date);
+    });
+
+  });
+
+  describe('#getCTime()', function() {
+
+    it('returns a date', function() {
+      var item = new Item('foo');
+      var date = item.getCTime();
+      assert.instanceOf(date, Date);
+      assert.isTrue(date <= new Date());
+    });
+
+  });
+
+  describe('#setCTime()', function() {
+
+    it('sets the ctime', function() {
+      var item = new Item('foo');
+      var date = new Date();
+      item.setCTime(date);
+      assert.equal(item.getCTime(), date);
+    });
+
+  });
+
+  describe('#getMTime()', function() {
+
+    it('returns a date', function() {
+      var item = new Item('foo');
+      var date = item.getMTime();
+      assert.instanceOf(date, Date);
+      assert.isTrue(date <= new Date());
+    });
+
+  });
+
+  describe('#setMTime()', function() {
+
+    it('sets the mtime', function() {
+      var item = new Item('foo');
+      var date = new Date();
+      item.setMTime(date);
+      assert.equal(item.getMTime(), date);
+    });
+
+  });
+
+  describe('#getMode()', function() {
+
+    it('returns a number', function() {
+      var item = new Item('foo');
+      assert.isNumber(item.getMode());
+    });
+
+  });
+
+  describe('#setMode()', function() {
+
+    it('sets the mode', function() {
+      var item = new Item('foo');
+      item.setMode(0644);
+      assert.equal(item.getMode(), 0644);
+    });
+
+  });
+
+
+
 });
