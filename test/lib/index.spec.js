c9d8e42a5b2e932e51f7ee6c51e8d95117bedc13
Tim Schaub <tim.schaub@gmail.com>
2013-11-18 09:34:04 -0700

    Tests for fs.exists and fs.existsSync

--- test/lib/index.spec.js
+++ test/lib/index.spec.js
@@ -115,6 +115,168 @@ describe('The API', function() {
 
 });
 
+describe('fs.exists(path, callback)', function() {
+
+  var fs;
+  beforeEach(function() {
+    fs = mock.fs({
+      'path/to/a.bin': new Buffer([1, 2, 3]),
+      'empty': {},
+      'nested': {
+        'dir': {
+          'file.txt': ''
+        }
+      }
+    });
+  });
+
+  it('calls with true if file exists', function(done) {
+    fs.exists(path.join('path', 'to', 'a.bin'), function(exists) {
+      assert.isTrue(exists);
+      done();
+    });
+  });
+
+  it('calls with true if directory exists', function(done) {
+    fs.exists('path', function(exists) {
+      assert.isTrue(exists);
+      done();
+    });
+  });
+
+  it('calls with true if empty directory exists', function(done) {
+    fs.exists('empty', function(exists) {
+      assert.isTrue(exists);
+      done();
+    });
+  });
+
+  it('calls with true if nested directory exists', function(done) {
+    fs.exists(path.join('nested', 'dir'), function(exists) {
+      assert.isTrue(exists);
+      done();
+    });
+  });
+
+  it('calls with true if file exists', function(done) {
+    fs.exists(path.join('path', 'to', 'a.bin'), function(exists) {
+      assert.isTrue(exists);
+      done();
+    });
+  });
+
+  it('calls with true if empty file exists', function(done) {
+    fs.exists(path.join('nested', 'dir', 'file.txt'), function(exists) {
+      assert.isTrue(exists);
+      done();
+    });
+  });
+
+  it('calls with false for bogus path', function(done) {
+    fs.exists(path.join('bogus', 'path'), function(exists) {
+      assert.isFalse(exists);
+      done();
+    });
+  });
+
+  it('calls with false for bogus path (II)', function(done) {
+    fs.exists(path.join('nested', 'dir', 'none'), function(exists) {
+      assert.isFalse(exists);
+      done();
+    });
+  });
+
+});
+
+
+describe('fs.existsSync(path)', function() {
+
+  var fs;
+  beforeEach(function() {
+    fs = mock.fs({
+      'path/to/a.bin': new Buffer([1, 2, 3]),
+      'empty': {},
+      'nested': {
+        'dir': {
+          'file.txt': ''
+        }
+      }
+    });
+  });
+
+  it('returns true if file exists', function() {
+    assert.isTrue(fs.existsSync(path.join('path', 'to', 'a.bin')));
+  });
+
+  it('returns true if directory exists', function() {
+    assert.isTrue(fs.existsSync('path'));
+  });
+
+  it('returns true if empty directory exists', function() {
+    assert.isTrue(fs.existsSync('empty'));
+  });
+
+  it('returns true if nested directory exists', function() {
+    assert.isTrue(fs.existsSync(path.join('nested', 'dir')));
+  });
+
+  it('returns true if file exists', function() {
+    assert.isTrue(fs.existsSync(path.join('path', 'to', 'a.bin')));
+  });
+
+  it('returns true if empty file exists', function() {
+    assert.isTrue(fs.existsSync(path.join('nested', 'dir', 'file.txt')));
+  });
+
+  it('returns false for bogus path', function() {
+    assert.isFalse(fs.existsSync(path.join('bogus', 'path')));
+  });
+
+  it('returns false for bogus path (II)', function() {
+    assert.isFalse(fs.existsSync(path.join('nested', 'dir', 'none')));
+  });
+
+});
+
+describe('fs.readdirSync(path)', function() {
+
+  var fs;
+  beforeEach(function() {
+    fs = mock.fs({
+      'path/to/file.txt': 'file content',
+      'nested': {
+        'sub': {
+          'dir': {
+            'one.txt': 'one content',
+            'two.txt': 'two content',
+            'empty': {}
+          }
+        }
+      }
+    });
+  });
+
+  it('lists directory contents', function() {
+    var items = fs.readdirSync(path.join('path', 'to'));
+    assert.isArray(items);
+    assert.deepEqual(items, ['file.txt']);
+  });
+
+  it('lists nested directory contents', function() {
+    var items = fs.readdirSync(path.join('nested', 'sub', 'dir'));
+    assert.isArray(items);
+    assert.deepEqual(items, ['empty', 'one.txt', 'two.txt']);
+  });
+
+  it('throws for bogus path', function() {
+    assert.throws(function() {
+      fs.readdirSync('bogus');
+    });
+  });
+
+});
+
+
 describe('fs.readdir(path, callback)', function() {
 
   var fs;
