2c2dfbe373fb0213a84048871f6bbd98f492d0a7
Tim Schaub <tim.schaub@gmail.com>
2014-01-06 17:22:50 -0700

    Allow EOF during read
    Previous to this, piped streams would continue to trickle one byte at a time toward the high water mark.

--- test/lib/index.spec.js
+++ test/lib/index.spec.js
@@ -2565,4 +2565,44 @@ describe('Mocking the file system', function() {
 
   });
 
+  describe('fs.createReadStream(path, [options])', function(done) {
+
+    var restore;
+    beforeEach(function() {
+      restore = mock({
+        'dir/source': 'source content'
+      });
+    });
+    afterEach(function() {
+      restore();
+    });
+
+    it('creates a readable stream', function() {
+
+      var stream = fs.createReadStream('dir/source');
+      assert.isTrue(stream.readable);
+
+    });
+
+    it('allows piping to a writable stream', function(done) {
+
+      var input = fs.createReadStream('dir/source');
+      var output = fs.createWriteStream('dir/dest');
+      output.on('close', function() {
+        fs.readFile('dir/dest', function(err, data) {
+          if (err) {
+            return done(err);
+          }
+          assert.equal(String(data), 'source content');
+          done();
+        });
+      });
+      output.on('error', done);
+
+      input.pipe(output);
+
+    });
+
+  });
+
 });
