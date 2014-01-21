c7ce49c800780be173eccf5c17042b2990c29d6e
Tim Schaub <tim.schaub@gmail.com>
2014-01-20 17:43:18 -0700

    Basic benchmarks

---
+++ benchmarks/tests/read-mock.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var fs = require('fs');
+
+var mock = require('../..');
+
+
+/**
+ * Test setup.  Not timed.
+ * @param {function(Error)} done Callback.
+ */
+exports.beforeEach = function(done) {
+  mock({
+    'foo-mock.txt': 'foo'
+  });
+  done();
+};
+
+
+/**
+ * Timed test.
+ * @param {function(Error)} done Callback.
+ */
+exports.test = function(done) {
+  fs.readFile('foo-mock.txt', 'utf8', function(err, str) {
+    assert.ifError(err);
+    assert.equal(str, 'foo');
+    done();
+  });
+};
+
+
+/**
+ * Test teardown.  Not timed.
+ * @param {function(Error)} done Callback.
+ */
+exports.afterEach = function(done) {
+  mock.restore();
+  done();
+};
