8bf0d3c2de395151aa836d1e62eb24ef28e45193
Tim Schaub <tim.schaub@gmail.com>
2018-08-05 23:14:58 -0600

    Implement binding.copyFile()

--- test/lib/index.spec.js
+++ test/lib/index.spec.js
@@ -604,6 +604,79 @@ describe('Mocking the file system', function() {
     });
   }
 
+  if (fs.copyFile && fs.copyFileSync) {
+    describe('fs.copyFile(src, dest[, flags], callback)', function() {
+      beforeEach(function() {
+        mock({
+          'path/to/src.txt': 'file content',
+          'path/to/other.txt': 'other file content',
+          empty: {}
+        });
+      });
+      afterEach(mock.restore);
+
+      it('copies a file to an empty directory', function(done) {
+        fs.copyFile('path/to/src.txt', 'empty/dest.txt', function(err) {
+          assert.isTrue(!err);
+          assert.isTrue(fs.existsSync('empty/dest.txt'));
+          assert.equal(
+            String(fs.readFileSync('empty/dest.txt')),
+            'file content'
+          );
+          done();
+        });
+      });
+
+      it('truncates dest file if it exists', function(done) {
+        fs.copyFile('path/to/src.txt', 'path/to/other.txt', function(err) {
+          assert.isTrue(!err);
+          assert.equal(
+            String(fs.readFileSync('path/to/other.txt')),
+            'file content'
+          );
+          done();
+        });
+      });
+
+      it('throws if dest exists and exclusive', function(done) {
+        fs.copyFile(
+          'path/to/src.txt',
+          'path/to/other.txt',
+          fs.constants.COPYFILE_EXCL,
+          function(err) {
+            assert.instanceOf(err, Error);
+            assert.equal(err.code, 'EEXIST');
+            done();
+          }
+        );
+      });
+
+      it('fails if src does not exist', function(done) {
+        fs.copyFile('path/to/bogus.txt', 'empty/dest.txt', function(err) {
+          assert.instanceOf(err, Error);
+          assert.equal(err.code, 'ENOENT');
+          done();
+        });
+      });
+
+      it('fails if dest path does not exist', function(done) {
+        fs.copyFile('path/to/src.txt', 'path/nope/dest.txt', function(err) {
+          assert.instanceOf(err, Error);
+          assert.equal(err.code, 'ENOENT');
+          done();
+        });
+      });
+
+      it('fails if dest is a directory', function(done) {
+        fs.copyFile('path/to/src.txt', 'empty', function(err) {
+          assert.instanceOf(err, Error);
+          assert.equal(err.code, 'EISDIR');
+          done();
+        });
+      });
+    });
+  }
+
   describe('fs.rename(oldPath, newPath, callback)', function() {
     beforeEach(function() {
       mock({
